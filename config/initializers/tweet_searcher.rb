6b1786d1a90ecb9946e21845e5a609948ec78ece
Benjamin Fleischer <github@benjaminfleischer.com>
2014-04-20 18:22:31 -0500

    Add configurable tweet app options

--- config/initializers/tweet_searcher.rb
+++ config/initializers/tweet_searcher.rb
@@ -1,1 +1,8 @@
 TWEET_SEARCHER = TwitterSearch.new(['RubyFriends', 'RubyFriend'])
+class TweetAppConfig
+  attr_accessor :hash_tags, :title
+end
+tweet_app           = TweetAppConfig.new
+tweet_app.hash_tags = '#RubyFriends'
+tweet_app.title     = '#RubyFriends'
+Rails.application.config.tweet_app = tweet_app
